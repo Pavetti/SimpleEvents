2cb89266154924c571203b0c14f14b257bffe354
Rigoletto <jacek15.jj@gmail.com>
2023-09-14 22:10:23 +0200

    first commit

---
+++ src/main/java/pl/pavetti/simpleevents/SimpleEvents.java
@@ -0,0 +1,17 @@
+package pl.pavetti.simpleevents;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleEvents extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
