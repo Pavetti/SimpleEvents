46d325bfa53a12ede73a3e8fa5d37afd7e892db0
Rigoletto <jacek15.jj@gmail.com>
2023-10-04 23:32:47 +0200

    added update checker

--- src/main/java/pl/pavetti/simpleevents/SimpleEvents.java
+++ src/main/java/pl/pavetti/simpleevents/SimpleEvents.java
@@ -1,10 +1,13 @@
 package pl.pavetti.simpleevents;
 
 import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.pavetti.simpleevents.api.MetricsLite;
+import pl.pavetti.simpleevents.api.UpdateChecker;
 import pl.pavetti.simpleevents.listener.CloseInventoryListener;
 import pl.pavetti.simpleevents.command.SimpleEventCommand;
 import pl.pavetti.simpleevents.config.ConfigFile;
@@ -36,6 +39,8 @@ public final class SimpleEvents extends JavaPlugin {
         registerTabCompleter();
         registerListener();
         registerTask();
+
+        updateCheck();
     }
 
     @Override
@@ -70,6 +75,16 @@ public final class SimpleEvents extends JavaPlugin {
 
     private void registerTask(){
         if(settings.isAutoActive()) new AutoEventStart(eventManager,settings).runTaskTimer(this,6000,settings.getInterval());
+    }
+
+    private void updateCheck(){
+        new UpdateChecker(this, 112876).getVersion(version -> {
+            if (this.getDescription().getVersion().equals(version)) {
+                getLogger().info("[SimpleEvents] There is not a new update available.");
+            } else {
+                Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[SimpleEvents] THERE IS A NEW UPDATE AVAILABLE!");
+            }
+        });
     }
 
     private boolean setupEconomy() {
